5a7e53f11972025651e54f7da0b79703acdf3856
Artem <temazasport@mail.ru>
2022-12-01 14:44:27 +0800

    Programm

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+if (size > 0)
+{
+
+    string[] stringArray = NewStringArrayInput(size);
+
+    
+    PrintArray(stringArray);
+
+    System.Console.WriteLine();
+
+    string[] newStringArray = SelectThreeSymbolString(stringArray);
+
+    
+    PrintArray(newStringArray);
+
+    
+}
+else
+{
+    Console.WriteLine("Ошибка. Колличество элементов не может быть отрицательным.");
+}
+
+string[] NewStringArrayInput(int size)
+{
+    string[] newStringArray = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i}-й элемент массива: ");
+        newStringArray[i] = Console.ReadLine();
+        //Console.WriteLine();
+    }
+
+    return newStringArray;
+}
+
+void PrintArray(string[] arrayToPrint)
+{
+    Console.Write("[ ");
+
+    for (int i = 0; i < arrayToPrint.Length - 1; i++)
+    {
+        Console.Write("\"" + arrayToPrint[i] +"\"" + " , ");
+    }
+
+    Console.Write("\"" + arrayToPrint[arrayToPrint.Length - 1] + "\"" + " ]");
+    Console.WriteLine();
+}
+
+string[] SelectThreeSymbolString(string[] stringArray)
+{
+    int size = stringArray.Length;
+    int newSize = 0;
+    int k = 0;
+    string[] newStringArray = new string[newSize];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            newSize++;
+            Array.Resize(ref newStringArray, newSize);
+            newStringArray[k] = stringArray[i];
+            k++;
+
+        }
+    }
+
+
+    return newStringArray;
+}
